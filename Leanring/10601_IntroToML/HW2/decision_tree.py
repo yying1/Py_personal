b1662f23b6c0da02e3e717169ea112d1d4b0ec4f
yying2 <yingyuefrank@hotmail.com>
2022-02-05 16:28:19 -0500

    10601 HW2

---
+++ Leanring/10601_IntroToML/HW2/decision_tree.py
@@ -0,0 +1,20 @@
+class Node:
+    '''
+    Here is an arbitrary Node class that will form the basis of your decision
+    tree. 
+    Note:
+        - the attributes provided are not exhaustive: you may add and remove
+        attributes as needed, and you may allow the Node to take in initial
+        arguments as well
+        - you may add any methods to the Node class if desired 
+    '''
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.attr = None
+        self.vote = None
+    
+
+
+if __name__ == '__main__':
+    pass
